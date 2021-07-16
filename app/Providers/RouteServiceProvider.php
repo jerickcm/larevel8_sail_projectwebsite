855fae3275003f4af7b9fa85934798a16dc4453e
Jerick C. Mangalus <jmangaluz@gmail.com>
2021-07-17 07:45:56 +0800

    update api rate limit

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -39,7 +39,6 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
 
-
             Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/test.php'));
@@ -70,7 +69,13 @@ class RouteServiceProvider extends ServiceProvider
     protected function configureRateLimiting()
     {
         RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
+            // return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
+            return Limit::perMinute(1000)->by(optional($request->user())->id ?: $request->ip());
+        });
+
+        RateLimiter::for('web', function (Request $request) {
+            // return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
+            return Limit::perMinute(1000)->by(optional($request->user())->id ?: $request->ip());
         });
     }
 }
